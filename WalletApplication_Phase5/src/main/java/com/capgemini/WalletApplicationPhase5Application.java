e23ca6b971f4ff41b4c7fb45945d5582acd8e233
Abhinav Singh bhadauria <46921338+Abhinav181997@users.noreply.github.com>
2019-03-07 10:50:38 +0530

    Add files via upload

---
+++ WalletApplication_Phase5/src/main/java/com/capgemini/WalletApplicationPhase5Application.java
@@ -0,0 +1,13 @@
+package com.capgemini;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalletApplicationPhase5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApplicationPhase5Application.class, args);
+	}
+
+}
